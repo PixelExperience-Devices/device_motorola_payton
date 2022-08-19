75ea951f99f3f5d26178f3874f832f0facf8efc3
Lucchetto <lucchetto.tie@live.com>
2022-08-19 10:48:18 +0200

    payton: copy prebuilt libhidlbase from vndk v32
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Change-Id: I22379311c81a5c95344ff27c6f12c1c1e7afe1ee

--- device.mk
+++ device.mk
@@ -517,3 +517,8 @@ PRODUCT_PACKAGES += \
     WifiOverlay \
     wpa_supplicant \
     wpa_supplicant.conf 
+
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhidlbase-v32.so \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
