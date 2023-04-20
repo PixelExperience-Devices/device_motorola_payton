5c28ffec52c84b190ea978f937a9362a84bffac1
Georg Veichtlbauer <georg@vware.at>
2023-04-20 21:32:39 +0200

    payton: Remove no longer needed Bluetooth cruft
    Change-Id: I645fd212cd47041c8d0e43565e6679095fa1adea

--- device.mk
+++ device.mk
@@ -91,7 +91,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth@1.0.vendor \
     audio.bluetooth.default \
-    libbt-vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
 # Boot animation
