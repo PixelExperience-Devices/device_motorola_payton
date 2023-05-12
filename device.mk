198c27d99fc60f27c02e2f819bd53b7238bc1529
Georg Veichtlbauer <georg@vware.at>
2023-05-12 10:08:46 +0200

    payton: Define PRODUCT_SHIPPING_API_LEVEL directly
    Change-Id: I096a8993abecdeea5fdfc6eef783087355d13461

--- device.mk
+++ device.mk
@@ -527,3 +527,5 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhidlbase-v32.so \
     prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
+
+PRODUCT_SHIPPING_API_LEVEL := 25
