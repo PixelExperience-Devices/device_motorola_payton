a96ac19d2b0c48d02fce22a9e5c3f5adf1732009
Georg Veichtlbauer <georg@vware.at>
2023-05-12 10:08:46 +0200

    payton: Define PRODUCT_SHIPPING_API_LEVEL directly
    Change-Id: I096a8993abecdeea5fdfc6eef783087355d13461

--- aosp_payton.mk
+++ aosp_payton.mk
@@ -18,7 +18,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
 # Inherit some common PixelExperience stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
