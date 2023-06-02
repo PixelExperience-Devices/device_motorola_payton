2394b78e77eb77f38ac3095526d4f4b5c864f919
Georg Veichtlbauer <georg@vware.at>
2023-06-02 10:12:57 +0200

    payton: sepolicy: Allow pocketmode to access fp sysfs
    Change-Id: I6f19cf47bb064519a2a0befc897560803731123f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -139,6 +139,7 @@ VENDOR_SECURITY_PATCH := 2021-02-01
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+PRODUCT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Treble
 BOARD_VNDK_VERSION := current
