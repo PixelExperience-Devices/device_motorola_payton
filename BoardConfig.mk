3c1648cffcf1ebdc65d5250c9ae4dabd6a0495fc
jro1979oliver <jroliveira.oliveira301@gmail.com>
2023-06-21 13:14:13 +0200

    payton: Use Product to call sepolicy instead of system_ext
    Change-Id: Icf01b31fdc0fd9d6480b748ad266aaddd81ebad7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -138,7 +138,7 @@ VENDOR_SECURITY_PATCH := 2021-02-01
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+PRODUCT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 PRODUCT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Treble
